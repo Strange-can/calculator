8ffd1ecac0fba8c0d0dc414a0f4e3b71f8b9873d
Strange-can <179990912+Strange-can@users.noreply.github.com>
2024-09-24 22:02:53 +0800

    Add calculate function to return answer to screen
    Inputting equations and pressing the "equal sign" button will return the answer on screen.
    Bug: after returning the answer, doing another equation with the previous answer won't work as expected.
    e.g 1 + 1 = 2 /new screen/ 2 + 1 = 1  (system makes num1(in this case the number '2') to 0)

--- javascript.js
+++ javascript.js
@@ -17,6 +17,8 @@ function divide(num1, num2) {
 let num1 = ''
 let num2 = ''
 let mathSymbol = ''
+let numbers = '1234567890'
+let symbols = '+-*/'
 
 function operator(num1, mathSymbol, num2) {
     if (mathSymbol === '+') {
@@ -32,9 +34,45 @@ function operator(num1, mathSymbol, num2) {
         return divide(num1, num2)
     }
 }
-
+let screenArray = []
 const screen = document.querySelector(".display")
 const seenButtons = document.querySelectorAll(".screenDisplay")
 for (const button of seenButtons) {
-    button.addEventListener("click", () => screen.textContent = button.textContent)
+    button.addEventListener("click", () => {screenArray.push(button.textContent)
+    screen.textContent = screenArray.join('')
+  })
 }
+
+function calculate() { 
+    let numStr1 = ''
+    let numStr2 = ''
+    for ( i = 0; i < screenArray.length; i++ ) {
+    if (symbols.includes(screenArray[i]) && screenArray.indexOf(screenArray[i]) !== 0 && mathSymbol === '') {
+        mathSymbol = screenArray[i]
+    }
+    else if ( mathSymbol === '' && (numbers.includes(screenArray[i]) || screenArray[i] === '-' ) ) {
+        numStr1 += screenArray[i]
+    }
+    else if ( numbers.includes(screenArray[i]) || (mathSymbol !== '' && symbols.includes(screenArray[i]))) {
+        numStr2 += screenArray[i]
+    }
+}
+    num1 = Number(numStr1)
+    num2 = Number(numStr2)
+    console.log(num1)
+    console.log(mathSymbol)
+    console.log(num2)
+    let answer = operator(num1, mathSymbol, num2)
+    screenArray.splice( 0, screenArray.length )
+    screenArray.push(answer)
+    screen.textContent = ''
+    screen.textContent = screenArray
+    num1 = screenArray[0]
+    mathSymbol = ''
+    num2 = ''
+
+}
+console.log(screenArray)
+const equalSign = document.querySelector(".equal")
+equalSign.addEventListener( "click", calculate)
+
